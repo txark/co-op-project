c45e5b62fb68c86eaf918e9e04d546bc3767b208
Artitaya-Khothisan <artitaya.tk@gmail.com>
2025-05-02 22:16:59 +0700

    admin_login, kku_login

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+// use App\Http\Controllers\KKU_Controller;
+// Route::post('/auth/login/reg-account', [KKU_Controller::class, 'register']);
+
 abstract class Controller
 {
     //
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 }); //call back f.
+
+Route::get('/kku-login', function () {
+    return view('kku_login');
+});
+
+Route::get('/admin-login', function () {
+    return view('admin_login');
+});
